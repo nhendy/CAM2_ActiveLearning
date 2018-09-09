8218a09095e4c75aa9b728598816c32e67f90650
nhendy <nhendy@purdue.edu>
2018-09-09 17:53:51 -0400

    try

--- main.py
+++ main.py
@@ -77,18 +77,18 @@ if  __name__ == '__main__':
     # #
     # #
     # #
-    # model_ft = model_ft.to(device)
-    #
-    # criterion = nn.CrossEntropyLoss()
-    #
-    # # Observe that all parameters are being optimized
-    # optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
-    #
-    # # Decay LR by a factor of 0.1 every 7 epochs
-    # exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
-    #
-    # model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-    #                        num_epochs=25)
+    model_ft = model_ft.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+
+    # Observe that all parameters are being optimized
+    optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+    # Decay LR by a factor of 0.1 every 7 epochs
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+    model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
+                           num_epochs=25)
 
 
 
