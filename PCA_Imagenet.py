1028089c575858887ee2a2ae7a51ebaaf2630d30
wang2170@purdue.edu <wang2170@purdue.edu>
2019-01-23 16:52:41 -0500

    All

--- PCA_Imagenet.py
+++ PCA_Imagenet.py
@@ -9,11 +9,11 @@ import matplotlib.cm as cm
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 from sklearn.preprocessing import StandardScaler
-from sklearn.datasets import load_digits
+#from sklearn.datasets import load_digits
 
 #Input the Data folder here
-BASE_DATA_FOLDER = "../home/data/ilsvrc/ILSVRC"#"../cat-and-dog"#../hymenoptera_data"#"../Medical_data"
-TRAin_DATA_FOLDER = os.path.join(BASE_DATA_FOLDER, "ILSVRC2012_Classification")
+BASE_DATA_FOLDER = "/home/data/ilsvrc/ILSVRC/ILSVRC2012_Classification"#"../cat-and-dog"#../hymenoptera_data"#"../Medical_data"
+TRAin_DATA_FOLDER = os.path.join(BASE_DATA_FOLDER, "train")
 
 #Plot
 def visualize_scatter(data_2d, label_ids, figsize=(10,10)):
@@ -28,35 +28,50 @@ def visualize_scatter(data_2d, label_ids, figsize=(10,10)):
                         data_2d[np.where(label_ids == label_id), 1],
                         marker='o',
                         color= 'red',
-                        linewidth='1',
-                        alpha=0.5,
-                        label=id_to_label_dict[label_id])
+                        linewidth='5',
+                        alpha=0.8,
+                        s=5)
+                        #label=id_to_label_dict[label_id])
         else:
             plt.scatter(data_2d[np.where(label_ids == label_id), 0],
                         data_2d[np.where(label_ids == label_id), 1],
                         marker='o',
-                        color=  plt.cm.Set1(label_id / float(nb_classes)),
+                        color= 'b', #plt.cm.Set1(label_id / float(nb_classes)),
                         linewidth='1',
-                        alpha=0.5,
-                        label=id_to_label_dict[label_id])
+                        alpha=0.8,
+                        s=1)
+                        #label=id_to_label_dict[label_id])
 
-    plt.legend(loc='best')
-    plt.show() 
-
+    #plt.legend(loc='best')
+    plt.savefig('T-SNE_IMAGENET.png') 
+    #plt.show()
 images = []
 labels = []
 
+
+class_counter=0
+class_limit=200
+image_counter=0
+image_limit=200
 #First resize the image to 200*200 with grey scale
 for class_folder_name in os.listdir(TRAin_DATA_FOLDER):
+    print(class_folder_name)
+    image_counter=0
+    class_counter = class_counter + 1
+    if (class_counter > class_limit):
+        break
     class_folder_path = os.path.join(TRAin_DATA_FOLDER, class_folder_name)
-    for image_path in glob(os.path.join(class_folder_path, "*.jpg")):
+    for image_path in glob(os.path.join(class_folder_path, "*.JPEG")):
+        image_counter = image_counter + 1
         image = cv2.imread(image_path, cv2.IMREAD_COLOR)
         image = cv2.resize(image, (100, 100))
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         image = image.flatten()
         images.append(image)
         labels.append(class_folder_name)
-        
+        if (image_counter > image_limit):
+            break
+       
 images = np.array(images)
 labels = np.array(labels)
 
@@ -78,15 +93,15 @@ images_scaled = StandardScaler().fit_transform(images)
 pca = PCA(n_components=2)
 pca_result = pca.fit_transform(images_scaled)
 pca_result_scaled = StandardScaler().fit_transform(pca_result)
-visualize_scatter(pca_result_scaled, label_ids)
+#visualize_scatter(pca_result_scaled, label_ids)
 #print(pca.explained_variance_ratio_)
 
 #print(pca_result.shape)
 #Based on https://distill.pub/2016/misread-tsne/, the perplexity value can significantly affect the output plot 
 
-#tsne = TSNE(n_components=2, perplexity=40.0)
-#tsne_result = tsne.fit_transform(pca_result)
-#tsne_result_scaled = StandardScaler().fit_transform(tsne_result)
-#visualize_scatter(tsne_result_scaled, label_ids)
+tsne = TSNE(n_components=2, perplexity=50.0)
+tsne_result = tsne.fit_transform(pca_result)
+tsne_result_scaled = StandardScaler().fit_transform(tsne_result)
+visualize_scatter(tsne_result_scaled, label_ids)
 
 
